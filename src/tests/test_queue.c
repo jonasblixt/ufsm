b3f437b9b2442571c27e98eafb230e94528b2ee2
Jonas Persson <jonpe960@gmail.com>
2018-06-03 21:55:19 +0200

    additional test for queue

--- src/tests/test_queue.c
+++ src/tests/test_queue.c
@@ -11,9 +11,14 @@
 #include <assert.h>
 #include "common.h"
 
+#include "gen/test_terminate_input.h"
+
 static bool flag_on_data = false;
 static bool flag_q_lock = false;
 static bool flag_q_unlock = false;
+
+void xA(void) {}
+void eA(void) {}
 
 void on_data(void)
 {
@@ -35,40 +40,41 @@ int main(int argc, char **argv)
     uint32_t err = UFSM_OK;
     uint32_t data[4];
     uint32_t i = 0;
+    struct ufsm_machine *m = get_StateMachine1();
 
-    struct ufsm_queue q;
-    assert (ufsm_queue_init(&q, 4, data) == UFSM_OK);
-
-
-    q.on_data = on_data;
-    q.lock = q_lock;
-    q.unlock = q_unlock;
+    struct ufsm_queue *q = ufsm_get_queue(m);
+    assert (ufsm_queue_init(q, 4, data) == UFSM_OK);
 
 
-    err = ufsm_queue_put(&q, 1);
+    q->on_data = on_data;
+    q->lock = q_lock;
+    q->unlock = q_unlock;
+
+
+    err = ufsm_queue_put(q, 1);
     assert (err == UFSM_OK);
-    err = ufsm_queue_get(&q, &i);
+    err = ufsm_queue_get(q, &i);
     assert ( err == UFSM_OK && i == 1);
 
-    err = ufsm_queue_get(&q, &i);
+    err = ufsm_queue_get(q, &i);
     assert ( err == UFSM_ERROR_QUEUE_EMPTY);
 
-    err = ufsm_queue_put(&q, 1);
-    err = ufsm_queue_put(&q, 2);
-    err = ufsm_queue_put(&q, 3);
-    err = ufsm_queue_put(&q, 4);
+    err = ufsm_queue_put(q, 1);
+    err = ufsm_queue_put(q, 2);
+    err = ufsm_queue_put(q, 3);
+    err = ufsm_queue_put(q, 4);
     
     assert (err == UFSM_OK);
-    err = ufsm_queue_put(&q, 5);
+    err = ufsm_queue_put(q, 5);
     assert (err == UFSM_ERROR_QUEUE_FULL);
 
-    err = ufsm_queue_get(&q, &i);
+    err = ufsm_queue_get(q, &i);
     assert ( err == UFSM_OK && i == 1);
-    err = ufsm_queue_get(&q, &i);
+    err = ufsm_queue_get(q, &i);
     assert ( err == UFSM_OK && i == 2);
-    err = ufsm_queue_get(&q, &i);
+    err = ufsm_queue_get(q, &i);
     assert ( err == UFSM_OK && i == 3);
-    err = ufsm_queue_get(&q, &i);
+    err = ufsm_queue_get(q, &i);
     assert ( err == UFSM_OK && i == 4);
 
     assert (flag_on_data && flag_q_unlock && flag_q_lock);
