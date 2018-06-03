ef81e41d5d62ca5c3ed3dc8d116b2184379a6fa5
Jonas Persson <jonpe960@gmail.com>
2018-06-03 21:44:44 +0200

    Added extra queue testing

--- src/tests/test_queue.c
+++ src/tests/test_queue.c
@@ -11,7 +11,24 @@
 #include <assert.h>
 #include "common.h"
 
+static bool flag_on_data = false;
+static bool flag_q_lock = false;
+static bool flag_q_unlock = false;
 
+void on_data(void)
+{
+    flag_on_data = true;
+}
+
+void q_lock(void)
+{
+    flag_q_lock = true;
+}
+
+void q_unlock(void)
+{
+    flag_q_unlock = true;
+}
 
 int main(int argc, char **argv)
 {
@@ -20,8 +37,12 @@ int main(int argc, char **argv)
     uint32_t i = 0;
 
     struct ufsm_queue q;
-
     assert (ufsm_queue_init(&q, 4, data) == UFSM_OK);
+
+
+    q.on_data = on_data;
+    q.lock = q_lock;
+    q.unlock = q_unlock;
 
 
     err = ufsm_queue_put(&q, 1);
@@ -50,6 +71,6 @@ int main(int argc, char **argv)
     err = ufsm_queue_get(&q, &i);
     assert ( err == UFSM_OK && i == 4);
 
-
+    assert (flag_on_data && flag_q_unlock && flag_q_lock);
     return 0;
 }
