4b717b45ceb067fcf166077f83b41529e642cedd
Jonas Blixt <jonpe960@gmail.com>
2024-09-08 09:24:54 +0200

    wip

--- ufsm/__main__.py
+++ ufsm/__main__.py
@@ -1,19 +1,21 @@
-from PySide6 import QtWidgets
-
 import sys
 
+from PySide6 import QtWidgets
+
+
 class MainWindow(QtWidgets.QMainWindow):
-
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
-
         self.setWindowTitle("Hello World")
         l = QtWidgets.QLabel("My simple app.")
         l.setMargin(10)
         self.setCentralWidget(l)
         self.show()
 
-def main():
+def main() -> None:
     app = QtWidgets.QApplication(sys.argv)
     w = MainWindow()
     app.exec()
+
+if __name__ == "__main__":
+    main()
