8017769100a2d5dbb39722224985d8e1c3a43f8e
Jonas Blixt <jonpe960@gmail.com>
2024-12-19 18:37:32 +0100

    wip: wip

---
+++ src/ufsm/gui/tool.py
@@ -0,0 +1,15 @@
+
+# begin
+# cancel
+# mouse_move_handler
+# mouse_press_handler
+# key_press_handler
+#
+# State:
+# 1. Select top left corner
+# 2. Select bottom right corner
+# 3. Done
+
+class Tool:
+    def __init__(self) -> None:
+        ...
